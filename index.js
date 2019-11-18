28edeab922db76d9b1796175723acce5fba56f3f
Jasmine <jasmine1226@gmail.com>
2019-11-17 21:16:18 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,52 @@
+function wakeDog(dogName, dogBreed) {
+  let result = `Wake ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+function leashDog(dogName, dogBreed) {
+  let result = `Leash ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+function walkToPark(dogName, dogBreed) {
+  let result = `Walk to the park with ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  let result = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+function walkHome(dogName, dogBreed) {
+  let result = `Walk home with ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+function unleashDog(dogName, dogBreed) {
+  let result = `Unleash ${dogName} the ${dogBreed}`;
+  console.log(result);
+  return result;
+}
+
+let routine = [
+  wakeDog,
+  leashDog,
+  walkToPark,
+  throwFrisbee,
+  walkHome,
+  unleashDog
+];
+
+function exerciseDog(dogName, dogBreed) {
+  let result = [];
+  for (let i = 0; i < routine.length; i++) {
+    result.push(routine[i](dogName, dogBreed));
+  }
+  return result;
+}
